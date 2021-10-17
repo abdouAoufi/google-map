242cfe4f9eae7ec5d69080f01bd857922d81c08e
Aoufi Abderhmane <48932498+abdouAoufi@users.noreply.github.com>
2021-10-18 00:32:54 +0100

    Update button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -5,7 +5,7 @@ import light from "../../assets/light.svg";
 export default function Button({ text }) {
   return (
     <button className="button">
-      <img className="location" src={light} height="25px" />
+      <img className="location" alt="light" src={light} height="25px" />
       {text}
     </button>
   );
